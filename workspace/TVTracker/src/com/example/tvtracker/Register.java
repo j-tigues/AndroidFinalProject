555065f669c4f9b97cd70bb7729a0b35186e885b
j-tigues <jartigues@smu.edu>
2013-04-30 21:59:27 -0500

    Android application uploaded

--- workspace/TVTracker/src/com/example/tvtracker/Register.java
+++ workspace/TVTracker/src/com/example/tvtracker/Register.java
@@ -3,13 +3,19 @@ package com.example.tvtracker;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class Register extends Activity {
+public class Register extends Activity implements OnClickListener{
 
+	Button register;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		register = (Button)findViewById(R.id.createUser);
+		register.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +23,14 @@ public class Register extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.register, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v==register){
+			
+		}
+		
 	}
 
 }
